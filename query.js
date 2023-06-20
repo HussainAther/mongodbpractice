b251f924113f24dd89081826dce606fa0fd07c21
Syed Hussain Ather <shussainather@gmail.com>
2023-06-20 12:22:38 -0400

    more queries

--- query.js
+++ query.js
@@ -1,2 +1,3 @@
 db.restaurants.find({}) /* Find restaurants within a database. */
 db.restaurant.find({}, { restaurant_id: 1, name: 1, borough: 1, cuisine: 1 }) /* Display the fields restaurant_id, name, borough and cuisine for all the documents in the collection restaurant. */
+db.restaurant.find({}, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 }) /* Display the fields restaurant_id, name, borough and cuisine, but exclude the field _id for all the documents in the collection restaurant. */
