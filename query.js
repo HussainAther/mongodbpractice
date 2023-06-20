2155a6618d283472fed71c79fbdb9864c2fe8f5b
Syed Hussain Ather <shussainather@gmail.com>
2023-06-20 12:18:37 -0400

    second find

--- query.js
+++ query.js
@@ -1,2 +1,2 @@
 db.restaurants.find({}) /* Find restaurants within a database. */
-
+db.restaurant.find({}, { restaurant_id: 1, name: 1, borough: 1, cuisine: 1 }) /* Display the fields restaurant_id, name, borough and cuisine for all the documents in the collection restaurant. */
